c25921f27f46dbffbe1da4d5b3f88bc64c4d119d
paren_s_povyazkoy <109079912+parnysha@users.noreply.github.com>
2024-10-11 23:17:33 +0300

    Update BookServiceImpl.java

--- springBookStore/src/main/java/com/example/springbookstore/service/BookServiceImpl.java
+++ springBookStore/src/main/java/com/example/springbookstore/service/BookServiceImpl.java
@@ -5,23 +5,19 @@ import com.example.springbookstore.exceptions.BookNotFoundException;
 import com.example.springbookstore.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
-
     @Override
     public String getBookInfo(final Long id) throws BookNotFoundException {
         final Optional<Book> bookOptional = bookRepository.findById(id);
 
         return bookOptional.map(Book::toString).orElseThrow(BookNotFoundException::new);
     }
-
     @Override
     public List<Book> getAllBooks() {
         bookRepository.findAll();
@@ -31,7 +27,6 @@ public class BookServiceImpl implements BookService {
                 .filter(book -> book.getAuthor()!=null)
                 .collect(Collectors.toList());
     }
-
     @Override
     public List<Book> getBooksByTitle(final String title) {
         return bookRepository.findByTitle(title)
@@ -46,20 +41,16 @@ public class BookServiceImpl implements BookService {
         bookRepository.flush();
         return addedBook;
     }
-
     @Override
     public List<Book> addBooks(List<Book> books) {
         final List<Book> addedBooks = bookRepository.saveAll(books);
         bookRepository.flush();
         return addedBooks;
     }
-
     @Override
     public boolean deleteBook(Book book) {
         final Optional<Book> bookOpt = bookRepository.findById(book.getId());
         bookRepository.delete(book);
         return bookOpt.isPresent();
     }
-
-    ;
 }
